a0f0874b2ab16f055c88023ff8b73c37b7344527
kerneyj <gamerguy9801@gmail.com>
2022-10-18 14:07:32 -0500

    formating

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -6,7 +6,6 @@ void kernel_main(void)
     /* Initialize terminal interface */
     terminal_initialize();
 	pg_init();
-
 	terminal_writestring("Hello World!\n");
 	terminal_writestring("Something to prove that it compiles\n");
 }
