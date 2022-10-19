396ea359bedb5a4f3c29cb751ec8f727abe18926
kerneyj <gamerguy9801@gmail.com>
2022-10-19 13:20:25 -0500

    printf hello world

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -6,10 +6,6 @@ void kernel_main(void)
 {
     /* Initialize terminal interface */
     term_init();
-	uint32_t cr3;
-	asm volatile ("mov %%cr3, %0" : "=r"(cr3));
-	printf("cr3: %x\n", cr3);
+	printf("Hello World\n");
 	pg_init();
-	asm volatile ("mov %%cr3, %0" : "=r"(cr3));
-	printf("cr3: %x\n", cr3);
 }
