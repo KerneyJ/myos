44cb586f5e50eac9ce40f83fdd71c42fdb98905b
kerneyJ <gamerguy9801@gmail.com>
2024-01-05 14:28:34 -0600

    commented out paging initialization

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -12,8 +12,8 @@ void kernel_main(uint32_t magic, uint32_t addr){
 	 */
 	term_init(); printf("[+]Terminal Initialized\n");
 
-	if(magic != MULTIBOOT_BOOTLOADER_MAGIC){
-		printf("[-]Magic number does not match multiboot magic\n");
-	}
-	pg_init(); printf("[+]Paging Initialized\n");
+//	if(magic != MULTIBOOT_BOOTLOADER_MAGIC){
+//		printf("[-]Magic number does not match multiboot magic\n");
+//	}
+//	pg_init(); printf("[+]Paging Initialized\n");
 }
