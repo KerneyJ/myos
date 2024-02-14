d75a16657b3e0318242ae707259f294ba781e7dc
kerneyJ <gamerguy9801@gmail.com>
2024-02-13 21:53:04 -0600

    currently blows up when looking at the multiboot framebuffer info

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,6 +4,147 @@
 #include "vm.h"
 #include "arch.h"
 
+uint32_t breakpoint_calls = 0;
+
+void breakpoint() __attribute__((noinline));
+void end() __attribute__((noinline));
+void mbt_cmdline(struct multiboot_tag* tag) __attribute__((noinline));
+void mbt_blname(struct multiboot_tag* tag) __attribute__((noinline));
+void mbt_module(struct multiboot_tag* tag) __attribute__((noinline));
+void mbt_bootdev(struct multiboot_tag* tag) __attribute__((noinline));
+void mbt_bmeminfo(struct multiboot_tag* tag) __attribute__((noinline));
+void mbt_mmap(struct multiboot_tag* tag) __attribute__((noinline));
+void* mbt_fb(struct multiboot_tag* tag) __attribute__((noinline));
+
+void breakpoint(){
+	breakpoint_calls++;
+}
+
+void end(){
+	breakpoint_calls++;
+}
+
+void mbt_cmdline(struct multiboot_tag* tag){
+	printf ("Command line = %s\n", ((struct multiboot_tag_string *) tag)->string);
+}
+
+void mbt_blname(struct multiboot_tag* tag){
+	printf ("Boot loader name = %s\n", ((struct multiboot_tag_string *) tag)->string);
+}
+
+void mbt_module(struct multiboot_tag* tag){
+	printf ("Module at 0x%x-0x%x. Command line %s\n",
+			((struct multiboot_tag_module *) tag)->mod_start,
+			((struct multiboot_tag_module *) tag)->mod_end,
+			((struct multiboot_tag_module *) tag)->cmdline);
+}
+
+void mbt_bmeminfo(struct multiboot_tag* tag){
+	printf ("mem_lower = %uKB, mem_uppe = %uKB\n",
+			((struct multiboot_tag_basic_meminfo *) tag)->mem_lower,
+			((struct multiboot_tag_basic_meminfo *) tag)->mem_upper);
+}
+
+void mbt_bootdev(struct multiboot_tag* tag){
+	printf ("Boot device 0x%x,%u,%u\n",
+			((struct multiboot_tag_bootdev *) tag)->biosdev,
+			((struct multiboot_tag_bootdev *) tag)->slice,
+			((struct multiboot_tag_bootdev *) tag)->part);
+
+}
+
+void mbt_mmap(struct multiboot_tag* tag){
+	multiboot_memory_map_t *mmap;
+	printf ("mmap\n");
+	for (mmap = ((struct multiboot_tag_mmap *) tag)->entries;
+		(multiboot_uint8_t *) mmap < (multiboot_uint8_t *) tag + tag->size;
+		mmap = (multiboot_memory_map_t *) ((unsigned long) mmap + ((struct multiboot_tag_mmap *) tag)->entry_size)){
+		printf (" base_addr = 0x%x%x,"
+				" length = 0x%x%x, type = 0x%x\n",
+				(unsigned) (mmap->addr >> 32),
+				(unsigned) (mmap->addr & 0xffffffff),
+				(unsigned) (mmap->len >> 32),
+				(unsigned) (mmap->len & 0xffffffff),
+				(unsigned) mmap->type);
+	}
+}
+
+void* mbt_fb(struct multiboot_tag* tag){
+	multiboot_uint32_t color;
+	unsigned i;
+	struct multiboot_tag_framebuffer *tagfb = (struct multiboot_tag_framebuffer *) tag;
+	void *fb = (void *)tagfb->common.framebuffer_addr;
+
+	breakpoint();
+	switch (tagfb->common.framebuffer_type){
+		case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
+			{
+				unsigned best_distance, distance;
+				struct multiboot_color *palette;
+				palette = tagfb->framebuffer_palette;
+				color = 0;
+				best_distance = 4*256*256;
+				for (i = 0; i < tagfb->framebuffer_palette_num_colors; i++){
+					distance = (0xff - palette[i].blue)
+						* (0xff - palette[i].blue)
+						+ palette[i].red * palette[i].red
+						+ palette[i].green * palette[i].green;
+
+					if (distance < best_distance){
+						color = i;
+						best_distance = distance;
+					}
+				}
+			}
+			break;
+		case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
+			color = ((1 << tagfb->framebuffer_blue_mask_size) - 1) << tagfb->framebuffer_blue_field_position;
+			break;
+
+		case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
+			color = '\\' | 0x0100;
+			break;
+
+		default:
+			color = 0xffffffff;
+			break;
+	}
+	breakpoint();
+
+	for (i = 0; i < tagfb->common.framebuffer_width && i < tagfb->common.framebuffer_height; i++){
+		switch (tagfb->common.framebuffer_bpp){
+			case 8:
+				{
+					multiboot_uint8_t *pixel = fb + tagfb->common.framebuffer_pitch * i + i;
+					*pixel = color;
+				}
+				break;
+			case 15:
+			case 16:
+				{
+					multiboot_uint16_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 2 * i;
+					*pixel = color;
+				}
+				break;
+			case 24:
+				{
+					multiboot_uint32_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 3 * i;
+					*pixel = (color & 0xffffff) | (*pixel & 0xff000000);
+				}
+				break;
+			case 32:
+				{
+					multiboot_uint32_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 4 * i;
+					*pixel = color;
+				}
+				break;
+		}
+	}
+	breakpoint();
+	return fb;
+}
+
+
 void kernel_main(uint32_t magic, uint32_t addr){
 		/*
 	 * [+] success
@@ -21,124 +162,37 @@ void kernel_main(uint32_t magic, uint32_t addr){
 	if (addr & 7) // unaligned multiboot info struct
 		goto failure;
 
+	breakpoint();
 	size = *(unsigned *) addr;
 	for (tag = (struct multiboot_tag *) (addr + 8); tag->type != MULTIBOOT_TAG_TYPE_END;
 		tag = (struct multiboot_tag *) ((multiboot_uint8_t *) tag + ((tag->size + 7) & ~7))){
+		breakpoint();
 		switch (tag->type){
 			case MULTIBOOT_TAG_TYPE_CMDLINE:
-				printf ("Command line = %s\n", ((struct multiboot_tag_string *) tag)->string);
+				mbt_cmdline(tag); // no triple fault 1st
 				break;
 			case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
-				printf ("Boot loader name = %s\n", ((struct multiboot_tag_string *) tag)->string);
+				mbt_blname(tag); // no triple fault 2nd
 				break;
 			case MULTIBOOT_TAG_TYPE_MODULE:
-				printf ("Module at 0x%x-0x%x. Command line %s\n",
-						((struct multiboot_tag_module *) tag)->mod_start,
-						((struct multiboot_tag_module *) tag)->mod_end,
-						((struct multiboot_tag_module *) tag)->cmdline);
+				mbt_module(tag);
 				break;
 			case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
-				printf ("mem_lower = %uKB, mem_upper = %uKB\n",
-						((struct multiboot_tag_basic_meminfo *) tag)->mem_lower,
-						((struct multiboot_tag_basic_meminfo *) tag)->mem_upper);
+				mbt_bmeminfo(tag); // no triple fault 4th
 				break;
 			case MULTIBOOT_TAG_TYPE_BOOTDEV:
-				printf ("Boot device 0x%x,%u,%u\n",
-						((struct multiboot_tag_bootdev *) tag)->biosdev,
-						((struct multiboot_tag_bootdev *) tag)->slice,
-						((struct multiboot_tag_bootdev *) tag)->part);
+				mbt_bootdev(tag); // no triple fault 5th
 				break;
 			case MULTIBOOT_TAG_TYPE_MMAP:
-				{
-					multiboot_memory_map_t *mmap;
-					printf ("mmap\n");
-					for (mmap = ((struct multiboot_tag_mmap *) tag)->entries;
-							(multiboot_uint8_t *) mmap < (multiboot_uint8_t *) tag + tag->size;
-							mmap = (multiboot_memory_map_t *) ((unsigned long) mmap + ((struct multiboot_tag_mmap *) tag)->entry_size))
-						printf (" base_addr = 0x%x%x,"
-								" length = 0x%x%x, type = 0x%x\n",
-								(unsigned) (mmap->addr >> 32),
-								(unsigned) (mmap->addr & 0xffffffff),
-								(unsigned) (mmap->len >> 32),
-								(unsigned) (mmap->len & 0xffffffff),
-								(unsigned) mmap->type);
-				}
+				mbt_mmap(tag); // no triple fault 3rd
 				break;
 			case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
-				{
-					multiboot_uint32_t color;
-					unsigned i;
-					struct multiboot_tag_framebuffer *tagfb = (struct multiboot_tag_framebuffer *) tag;
-					fb = (void *) (unsigned long) tagfb->common.framebuffer_addr;
-					switch (tagfb->common.framebuffer_type){
-						case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
-							{
-								unsigned best_distance, distance;
-								struct multiboot_color *palette;
-								palette = tagfb->framebuffer_palette;
-								color = 0;
-								best_distance = 4*256*256;
-								for (i = 0; i < tagfb->framebuffer_palette_num_colors; i++){
-									distance = (0xff - palette[i].blue)
-										* (0xff - palette[i].blue)
-										* + palette[i].red * palette[i].red
-										* + palette[i].green * palette[i].green;
-									if (distance < best_distance){
-										color = i;
-										best_distance = distance;
-									}
-								}
-							}
-							break;
-
-						case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
-							color = ((1 << tagfb->framebuffer_blue_mask_size) - 1) << tagfb->framebuffer_blue_field_position;
-							break;
-
-						case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
-							color = '\\' | 0x0100;
-							break;
-
-						default:
-							color = 0xffffffff;
-							break;
-					}
-
-					for (i = 0; i < tagfb->common.framebuffer_width && i < tagfb->common.framebuffer_height; i++){
-						switch (tagfb->common.framebuffer_bpp){
-							case 8:
-								{
-									multiboot_uint8_t *pixel = fb + tagfb->common.framebuffer_pitch * i + i;
-									*pixel = color;
-								}
-								break;
-							case 15:
-							case 16:
-								{
-									multiboot_uint16_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 2 * i;
-									*pixel = color;
-								}
-								break;
-							case 24:
-								{
-									multiboot_uint32_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 3 * i;
-									*pixel = (color & 0xffffff) | (*pixel & 0xff000000);
-								}
-								break;
-							case 32:
-								{
-									multiboot_uint32_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 4 * i;
-									*pixel = color;
-								}
-								break;
-						}
-					}
-					break;
-				}
+				fb = mbt_fb(tag); // 6th causes triple fault
 				break;
 		}
 	}
 
+//	breakpoint();
 	uint16_t* fba = (uint16_t *)fb;
 	for(int i = 0; i < 80; i++){
 		for(int j = 0; j < 25; j++){
@@ -146,6 +200,8 @@ void kernel_main(uint32_t magic, uint32_t addr){
 			fba[index] = 'C';
 		}
 	}
+	end();
+	printf("Number of calls to breakpoints %i\n", breakpoint_calls);
 	return; // jump out of kernl_main to scheduler
 
 failure:
