61ab368a764c101a22ff84e39f29756b74b31558
kerneyJ <gamerguy9801@gmail.com>
2024-07-22 10:37:40 -0500

    fixed alloc page

--- src/arch/x86_64/early_mem.c
+++ src/arch/x86_64/early_mem.c
@@ -7,18 +7,37 @@ uint64_t earlymem_init(){
     /* Mark first 6 MiB as used in bitmap */
     for(int i = 0; i < 24; i++)
         page_bitmap[i] = 0xffffffffffffffff;
+    page_bitmap[24] = 0xfffffffffffffffe;
+    /* map page for the framebuffer */
+    /* return map_page_earlymem(0xfd000000, 0, 0); */
+    uint64_t page = alloc_page_earlymem();
+    page_bitmap[25] = page;
     return 0;
 }
 
 uint64_t alloc_page_earlymem(){
-	uint64_t index, bit;
-	for(index = 0; index < 4096; index++){
-		for(bit = 0; bit < 64; bit++){
-			if(!(page_bitmap[index] & (1 << bit)))
-				break;
-		}
-	}
-	return PAGE_SIZE * (index * 64 + bit);// the base physical address of the page
+    /* In future maybe have static variable or smth that caches
+     * the location of free pages
+     * or have some sort of data structure to make this faster
+     */
+    uint64_t page, width = 64, mask = 0xffffffffffffffff, index;
+    for(index = 0; index < 32768; index++){
+        if(mask != page_bitmap[index]){
+            break;
+        }
+    }
+    page = page_bitmap[index];
+    while(width > 1){
+        if((mask & page) != mask){
+            width /= 2;
+            mask = mask & (mask << width);
+        }
+        else
+            mask = mask >> width;
+    }
+    if((mask & page) == mask)
+        mask >>= 1;
+    return (index * 64 + mask) * PAGE_SIZE; // the base physical address of the page
 }
 
 uint64_t free_page_earlymem(uint64_t paddr){
@@ -30,23 +49,27 @@ uint64_t free_page_earlymem(uint64_t paddr){
 }
 
 uint64_t map_page_earlymem(uint64_t vaddr, uint64_t paddr, uint64_t flags){
-	/* Assumes 2 MiB page size
-	 * TODO Make this coordinate with boot.S for abitrary page sizes
-	 * Assumes that we are looking to extend pml2 since its early
-	 */
-	uint64_t pml3_index = (vaddr >> 29) & 0x1ff;
-	uint64_t pml2_index = (vaddr >> 20) & 0x1ff;
+    // writing stuff to page_bitmap[24] for debuging
+    uint64_t pml4_index = (vaddr >> 39) & 0x1ff;
+    uint64_t pml3_index = (vaddr >> 30) & 0x1ff;
+    uint64_t pml2_index = (vaddr >> 21) & 0x1ff;
+    uint64_t pml1_index = (vaddr >> 12) & 0x1ff;
+    uint64_t *pml;
 
-	/*TODO check if pml2 at pml2_index index is present
-	 * if not map it
-	 */
-	if(!(pml3[pml3_index] & 0x1)){
-		uint64_t new_pml2_addr = alloc_page_earlymem();
-		// TODO have to map this new page
-		// TODO write this into the pml3 page table
-	}
+    pml = (uint64_t*)((uint64_t)pml4 & 0xfffffffff000);
+    if(!(pml[pml4_index] & PG_PRESENT))
+        return 4;
 
-	if(!(pml2[pml2_index] & 0x1)){
-	}
-	return 0;
+    pml = (uint64_t*)((uint64_t)pml3 & 0xfffffffff000);
+    if(!(pml[pml3_index] & PG_PRESENT)){
+        uint64_t temp_paddr = alloc_page_earlymem();
+        page_bitmap[26] = temp_paddr;
+        return 3;
+    }
+
+    pml = (uint64_t*)((uint64_t)pml2 & 0xfffffffff000);
+    if(!(pml[pml2_index] & PG_PRESENT))
+        return 2;
+
+    return 0;
 }
