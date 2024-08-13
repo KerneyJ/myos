d72e5e167ccfdd3f029b6f9c8c06467ff2a215e0
kerneyJ <gamerguy9801@gmail.com>
2024-08-13 12:25:21 -0500

    tab -> space

--- src/lib/string/memcpy.c
+++ src/lib/string/memcpy.c
@@ -2,8 +2,8 @@
 
 void* memcpy(void* restrict dst, const void* restrict src, size_t n){
     unsigned char* ucdst = (unsigned char*)dst;
-	unsigned char* ucsrc = (unsigned char*)src;
-	for(size_t i = 0; i < n; i++)
-		ucdst[i] = ucsrc[i];
-	return dst;
+    unsigned char* ucsrc = (unsigned char*)src;
+    for(size_t i = 0; i < n; i++)
+        ucdst[i] = ucsrc[i];
+    return dst;
 }
